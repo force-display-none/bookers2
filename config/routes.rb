d3818f5011ad92581824222097ffb118d6c63ae9
kckw0917 <kckw0917@gmail.com>
2018-06-21 16:42:44 +0900

    topページ作りました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get '/' => 'users#show'
+  get '/' => 'books#top'
+  # get '/' => 'devise/sessions#new'
   resources :users, only: [:show]
-  resources :post_images, only: [:new, :create, :index, :show]
+  # resources :post_images, only: [:new, :create, :index, :show]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
